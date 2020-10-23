63a84653bc3ca46977531ec65ca9d4a174d19d31
Saeed Tavakoli <sti408@gmail.com>
2020-10-23 13:42:56 +0330

    commit change add port

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const app = express();
+const port = 3000
 
 app.get('/',(req,res) =>{
     res.send('Hello!');
 
 })
 
-app.listen(3000, () =>{
-console.log('Server is runnig on Port 3000');
+app.listen(port, () =>{
+console.log('Server is runnig on Port ${port}');
 });
